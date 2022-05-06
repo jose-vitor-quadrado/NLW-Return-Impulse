85f8e62998a149425bfa666736fe7957b1db3a11
Jose Vitor Quadrado <josevitorquadrado@gmail.com>
2022-05-06 15:08:06 -0300

    dia 4

---
+++ mobile/App.tsx
@@ -0,0 +1,40 @@
+import 'react-native-gesture-handler'
+import { StatusBar } from 'expo-status-bar'
+import { StyleSheet, Text, View } from 'react-native'
+import AppLoading from 'expo-app-loading'
+import { 
+  useFonts, 
+  Inter_400Regular, 
+  Inter_500Medium 
+} from '@expo-google-fonts/inter'
+import { theme } from './src/theme'
+import Widget from './src/components/Widget'
+
+export default function App() {
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_500Medium
+  })
+
+  if (!fontsLoaded) {
+    return <AppLoading />
+  }
+
+  return (
+    <View style={ styles.container }>
+      <Widget />
+      <StatusBar 
+        style="light" 
+        backgroundColor="transparent"
+        translucent
+      />
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: theme.colors.background
+  },
+})
---
+++ mobile/src/components/ScreenshotButton/index.tsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import { View, TouchableOpacity, Image } from 'react-native'
+import { Trash, Camera } from 'phosphor-react-native'
+import { styles } from './styles'
+import { theme } from '../../theme'
+
+interface SubscribeButtonProps {
+  screenshot: string | null
+  onTakeScreenshot: () => void
+  onRemoveScreenshot: () => void
+}
+
+export function ScreenshotButton({ screenshot, onTakeScreenshot, onRemoveScreenshot }: SubscribeButtonProps) {
+  return (
+    <TouchableOpacity 
+      style={ styles.container }
+      onPress={ screenshot ? onRemoveScreenshot : onTakeScreenshot }
+    >
+      {
+        screenshot 
+          ? 
+          <View>
+            <Image 
+              style={ styles.image }
+              source={ { uri: screenshot } }
+            />
+            <Trash
+              size={ 22 }
+              color={ theme.colors.text_secondary }
+              weight="fill"
+              style={ styles.removeIcon }
+            />
+          </View>
+          :
+          <Camera
+            size={ 24 }
+            color={ theme.colors.text_primary }
+            weight="bold"
+          />
+      }
+    </TouchableOpacity>
+  )
+}
